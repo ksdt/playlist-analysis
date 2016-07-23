875267524e3cdf21a7c6756544eb4810d28f9df8
Tennyson Holloway <tennyson.holloway@gmail.com>
2016-07-22 20:05:58 -0700

    refactored... a lot

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var web = require('./lib/web');
+var web = require('./lib/web'),
+    shows = require('./lib/show.js'),
+    spotify = require('./lib/spotify.js'),
+    spinitron = require('./lib/spinitron.js');
 
-
-web.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+Promise.all([
+    spinitron.init(),
+    spotify.init()
+])
+.then(() => {
+    web.listen(process.env['PORT'] || 3000, function () {
+      console.log('Example app listening on port 3000!');
+    });
+}, error => console.log(error))
+.catch(error => console.log.bind(console));
